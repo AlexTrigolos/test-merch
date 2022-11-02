72287e9a949fe6f7f84ec7c408f144cec3b46a8c
Teslagirl69 <teslagirl17@google.com>
2022-11-02 12:31:26 +0300

    add new migrations and models

---
+++ app/models/employee_group.rb
@@ -0,0 +1,2 @@
+class EmployeeGroup < ApplicationRecord
+end
---
+++ db/migrate/20221102091810_create_employee_groups.rb
@@ -0,0 +1,9 @@
+class CreateEmployeeGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :employee_groups do |t|
+      t.string :group_name, unique: true
+      t.references :user, foreign_key: true, null: true
+      t.timestamps
+    end
+  end
+end
