55cd753b69766cae8fa601d66205d3088b7a7faf
Teslagirl69 <teslagirl17@google.com>
2022-11-02 12:30:08 +0300

    delete old migrations

--- db/migrate/20221101094536_add_comments.rb
+++
@@ -1,9 +0,0 @@
- class AddComments < ActiveRecord::Migration[7.0]
-  def change
-    create_table :comments do |t|
-      t.references :user, null: false, foreign_key: true
-      t.text :body
-      t.references :commentable, polymorphic: true, index: true
-    end
-  end
-end
--- db/migrate/20221101112737_add_merches.rb
+++
@@ -1,9 +0,0 @@
- class AddMerches < ActiveRecord::Migration[7.0]
-  def change
-    create_table :merches do |t|
-      t.string :merch_name
-      t.references :category, foreign_key: true
-      t.timestamps
-    end
-  end
-end
