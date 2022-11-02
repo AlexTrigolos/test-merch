80af09ff7c7f8527c1127b8c179f95ebe47ec6b1
Teslagirl69 <teslagirl17@google.com>
2022-11-02 13:52:45 +0300

    update models

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,4 @@
 class Stock < ApplicationRecord
+  has_many :stock_items
+  belongs_to :user
 end
--- app/models/stock_item_action.rb
+++ app/models/stock_item_action.rb
@@ -1,2 +1,6 @@
 class StockItemAction < ApplicationRecord
+  belongs_to :employee
+  belongs_to :employee_group
+  belongs_to :stock_item
+  has_many :comments, as: :commentable
 end
